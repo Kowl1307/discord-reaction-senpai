311b09e855d3ab66cea905dfd66ab11fb7ed2d99
Kilian <63902217+Kowl1307@users.noreply.github.com>
2021-05-01 16:13:57 +0200

    Update const.py

--- const.py
+++ const.py
@@ -3,6 +3,6 @@ DESCRIPTION = "This bot creates embeds with "
 #Time to answer when creating an embed (in seconds)
 TIMEOUT = 60
 #insert your bot token here
-TOKEN = "ODA1MTgyMDMyNDEzODUxNjY4.YBXKNA.i1YrCaq-c2S6qmk5gJKcr-IKl70"
+TOKEN = ""
 #Command Prefix
 CMDPREFIX = "!"
